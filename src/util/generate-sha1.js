8db82ab3a1cf4e788e566dd45952689c7e24d470
allenmichael <allenmichaelgrobelny@gmail.com>
2020-01-27 13:08:08 -0600

    Updated SHA logic in chunked uploader to update overall file digest per chunk. Broke out base64 logic from generate-sha utility and changed generate-sha utility to return hash and array buffer of file or file chunk.

--- src/util/generate-sha1.js
+++ src/util/generate-sha1.js
@@ -1,5 +1,6 @@
 // 'use strict';
 import Rusha from 'rusha';
+import Base64ArrayBuffer from './base64-array-buffer'
 import CheckForFileReader from './check-for-file-reader';
 export default (blob) => {
     return new Promise((resolve, reject) => {
@@ -12,7 +13,10 @@ export default (blob) => {
                     return;
                 }
                 var rusha = new Rusha();
-                resolve(base64ArrayBuffer(rusha.rawDigest(evt.target.result).buffer));
+                resolve({
+                    hash: Base64ArrayBuffer(rusha.rawDigest(evt.target.result).buffer),
+                    arrayBuffer: evt.target.result
+                });
 
             };
             reader.readAsArrayBuffer(blob);
@@ -20,45 +24,4 @@ export default (blob) => {
             reject(e);
         }
     });
-}
-
-function base64ArrayBuffer(arrayBuffer) {
-    var base64 = ''
-    var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-
-    var bytes = new Uint8Array(arrayBuffer)
-    var byteLength = bytes.byteLength
-    var byteRemainder = byteLength % 3
-    var mainLength = byteLength - byteRemainder
-
-    var a, b, c, d
-    var chunk
-
-    for (var i = 0; i < mainLength; i = i + 3) {
-        chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
-
-        a = (chunk & 16515072) >> 18
-        b = (chunk & 258048) >> 12
-        c = (chunk & 4032) >> 6
-        d = chunk & 63
-
-        base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d]
-    }
-
-    if (byteRemainder == 1) {
-        chunk = bytes[mainLength]
-
-        a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
-        b = (chunk & 3) << 4 // 3   = 2^2 - 1
-        base64 += encodings[a] + encodings[b] + '=='
-    } else if (byteRemainder == 2) {
-        chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
-
-        a = (chunk & 64512) >> 10
-        b = (chunk & 1008) >> 4
-        c = (chunk & 15) << 2
-        base64 += encodings[a] + encodings[b] + encodings[c] + '='
-    }
-
-    return base64
 }
